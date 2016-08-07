163887727c5ac7203c224cb5461ce481c5e94672
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2016-08-08 09:51:49 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# react-isolated-scroll
+
+React component for [isolated-scroll](https://github.com/markdalgleish/isolated-scroll).
+
+```bash
+$ npm install --save react-isolated-scroll
+```
+
+## Usage
+
+```js
+import IsolatedScroll from 'react-isolated-scroll';
+
+<IsolatedScroll>
+  {/* children... */}
+</IsolatedScroll>
+```
+
+## Todo
+
+- Add demo.
+- Add tests.
+
+## License
+
+[MIT](https://markdalgleish.mit-license.org/)
---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React, { Component, PropTypes } from 'react';
+import isolatedScroll from 'isolated-scroll';
+
+const unbindHandlersKey = '__unbind_handlers__';
+
+export default class IsolatedScroll extends Component {
+
+  static propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node
+    ])
+  };
+
+  constructor() {
+    super();
+
+    this.storeComponentReference = this.storeComponentReference.bind(this);
+  }
+
+  componentDidMount() {
+    this[unbindHandlersKey] = isolatedScroll(this.component);
+  }
+
+  componentWillUnmount() {
+    this[unbindHandlersKey]();
+  }
+
+  storeComponentReference(component) {
+    if (component !== null) {
+      this.component = component;
+    }
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <div ref={this.storeComponentReference} {...this.props}>
+        { children }
+      </div>
+    );
+  }
+
+}
