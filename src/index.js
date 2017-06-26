330a0a9ab0a8eb9d5c473f71729abad68e4981ce
Misha Moroshko <michael.moroshko@gmail.com>
2017-06-26 15:14:20 -0700

    Fix PropTypes deprecation warning with React 15.5.0 (#4)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import isolatedScroll from 'isolated-scroll';
 
 const unbindHandlersKey = '__unbind_handlers__';
