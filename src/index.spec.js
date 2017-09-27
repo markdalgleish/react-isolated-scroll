80d1be263c539bc4be310eccb5faee7d5e842a66
Jahred Hope <jhope@seek.com.au>
2017-09-27 21:28:00 +1000

    Upgrade jest version

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,5 +1,5 @@
 import IsolatedScroll from './index'
-import renderer from 'react-test-renderer';
+import ReactTestRenderer from 'react-test-renderer';
 import React from 'react'
 
 jest.mock('isolated-scroll')
@@ -13,30 +13,30 @@ describe('IsolatedScroll', () => {
     expect(IsolatedScroll).toBeInstanceOf(Function)
   })
   it('should mount isolatedScroll binding', () => {
-    renderer.create(<IsolatedScroll />)
+    ReactTestRenderer.create(<IsolatedScroll />)
     expect(mockedIsolatedScroll).toHaveBeenCalledTimes(1)
   })
   it('should unmount isolatedScroll binding', () => {
     const mockedUnbindHandlersKey = jest.fn()
     mockedIsolatedScroll.mockImplementation(() => mockedUnbindHandlersKey)
-    const instance = renderer.create(<IsolatedScroll />)
+    const instance = ReactTestRenderer.create(<IsolatedScroll />)
     instance.unmount()
     expect(mockedUnbindHandlersKey).toHaveBeenCalledTimes(1)
   })
   it('should render', () => {
-    const tree = renderer.create(
+    const tree = ReactTestRenderer.create(
       <IsolatedScroll />
     ).toJSON();
     expect(tree).toMatchSnapshot();
   })
   it('should pass through children', () => {
-    const tree = renderer.create(
+    const tree = ReactTestRenderer.create(
       <IsolatedScroll>exampleChildTextNode</IsolatedScroll>
     ).toJSON();
     expect(tree).toMatchSnapshot();
   })
   it('should pass through attributes', () => {
-    const tree = renderer.create(
+    const tree = ReactTestRenderer.create(
       <IsolatedScroll data-example-attribute="exampleValue" />
     ).toJSON();
     expect(tree).toMatchSnapshot();
